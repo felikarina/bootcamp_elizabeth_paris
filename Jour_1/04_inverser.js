e3f7b43fe51175eb20ad63f710287dba92e0c192
Karina <146747979+felikarina@users.noreply.github.com>
2024-05-27 10:43:14 +0200

    inverser

--- Jour_1/04_inverser.js
+++ Jour_1/04_inverser.js
@@ -1,4 +1,4 @@
-## 🔂 Inverser
+/* 🔂 Inverser
 
 [Source: Newsletter de Cassidoo du 15/01](https://buttondown.email/cassidoo/archive/try-and-fail-but-dont-fail-to-try-john-quincy/)
 
@@ -8,13 +8,13 @@ Exemple
 
 Pour ce tableau:
 
-```
+```*/
 let array = [
 [1,2,3],
 [4,5,6],
 [7,8,9]
 ]
-```
+/*```
 En mode "horizontal", on obtient
 
 ```
@@ -27,4 +27,29 @@ Tandis qu'en mode "vertical", on obtient
 ```
 > flip(array, 'vertical')
 > [[7,8,9],[4,5,6],[1,2,3]]
-```
+```*/
+
+function flip_horizontal(array){
+    let result = []
+    let line = []
+    for(i=0; i<array.length; i++){
+        line = array[i].reverse()
+        let push = result.push(line)
+    }
+    return result
+}
+
+function flip_vertical(array){
+    let result = []
+    let line = []
+    for(i=array.length-1; i>=0; i--){
+        line = array[i]
+        let push = result.push(line)
+    }
+    return result
+}
+
+console.log(flip_horizontal(array))
+console.log(flip_vertical(array))
+
+
